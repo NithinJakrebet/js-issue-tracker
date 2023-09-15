0f35b10118018b6bab65a73abc68678e8ea606d9
Nithin Jakrebet <nithinjbet@gmail.com>
2023-09-15 07:55:14 -0700

    set up more index.html, set up main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+// retrieve issues from the Local Storage
+function fetchIssues () {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    var issuesList = document.getElementById(issuesList);
+
+    issuesList.innerHTML = '';
+
+    for (var i = 0; i < issues.length; i++) {
+        var id = issues[i].id;
+        var desc = issues[i].description;
+        var severity = issues[i].severity;
+        var assignedTo = issues[i].assignedTo
+        var status = issues[i].
+    }
+
+}
