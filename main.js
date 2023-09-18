29cae4849f362cfe95d82459b8ce7edb673aad2e
Nithin Jakrebet <nithinjbet@gmail.com>
2023-09-17 18:59:51 -0700

    little more work on main.js

--- main.js
+++ main.js
@@ -10,7 +10,18 @@ function fetchIssues () {
         var desc = issues[i].description;
         var severity = issues[i].severity;
         var assignedTo = issues[i].assignedTo
-        var status = issues[i].
+        var status = issues[i].status;
+
+        issuesList.innerHTML += '<div class = "well">' +
+                                '<h6>Issue ID: ' + id + '</h6>' +
+                                + status + '</span></p>' +
+                                '<h3>' + desc + '</h3>' +
+                                '<p><span class = "glyphicon glyphicon-time"></span> '
+                                + severity ' ' +
+                                '<span class = "glyphicon glyphicon -user"</span> ' + assignedTo + '</p>' +
+                                '<a href = "#" class = "btn btn-warning" onclick="setStatusClosed(\''+id+'\')">Delete</a>'
+                                '</div>';
+        
     }
 
 }
